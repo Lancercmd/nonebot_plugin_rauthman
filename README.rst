872adaafaab8d5ccc489bd9caf2af6649ca108f1
Lancercmd <lancercmd@gmail.com>
2022-01-08 21:58:14 +0800

    ⬆️ Update nonebot2 requirement from ^2.0.0-alpha.13 to ^2.0.0-beta.1

--- README.rst
+++ README.rst
@@ -1,99 +1,198 @@
-nonebot_plugin_rauthman
-========
+##############################################################################
+RAM - 基于规则的授权管理器
+##############################################################################
+******************************************************************************
+前言
+******************************************************************************
+| 本项目将对 PyPI 上的发行版 `nonebot-plugin-rauthman <https://pypi.org/project/nonebot-plugin-rauthman/>`_ 来源进行 deprecate 的处理。
+|
 
-- 基于 `nonebot / nonebot2 <https://github.com/nonebot/nonebot2>`_
+ 由于本模块的性质，未来即使提交更新也仅对本仓库进行。
 
-功能
---------
+ 正在使用的 PyPI 发行版来源也应择日改为使用本 Git 来源。
 
-- 基于规则的授权管理
+| 基于 `NoneBot2 <https://github.com/nonebot/nonebot2>`_。
 
+******************************************************************************
+Highlights
+******************************************************************************
+* 为 Matcher 或 MatcherGroup 配置一条或多条 Rule 来实现功能的授权管理
+
+.. code:: python
+
+ # MatcherGroup
+ from nonebot.plugin import MatcherGroup
+
+ from nonebot_plugin_rauthman import isInService
+
+ workers = MatcherGroup(
+     type="message", rule=isInService("module_name", 10)
+ )  # Rule 自动套用到 MatcherGroup 下所有 Matcher
+ worker_1 = workers.on_regex(...)
+ worker_2 = workers.on_regex(...)
+ worker_3 = workers.on_regex(...)
+ worker_4 = workers.on_regex(...)
+ worker_5 = workers.on_regex(...)
+ worker_6 = workers.on_regex(...)
+
+ ...
+.. code:: python
+
+ # Matcher
+ from nonebot.plugin import on_command
+
+ from nonebot_plugin_rauthman import isInService
+
+ worker = on_command(
+     "test", rule=isInService("module_name_A", 10) & isInService("module_name_B", 10)
+ )  # 同时满足多个 Rule 才可触发
+
+ ...
+
+* 授权策略可选 ``根据可用功能`` 或 ``根据服务级别``
+* 参数可完全自定义
+
+.. code:: python
+
+ @dataclass
+ class Options:
+     """
+     ###   RAM - 配置项
+     ###   参数
+     -     cmd: str  指令名，或者叫触发词
+     -     policy: int  授权策略，可选值如下：
+         -     0  根据可用功能
+         -     1  根据服务级别
+
+     ###   参数 - 根据可用功能
+     -     add: str  启用功能
+     -     rm: str  禁用功能
+     -     show: str  展示群功能状态
+     -     available: str  展示全局可用功能
+     """
+     permission: Permission = SUPERUSER
+     policy: int = getattr(config, "ram_policy") or 0
+     cmd: str = getattr(config, "ram_cmd") or "ram"
+     add: str = getattr(config, "ram_add") or "-a"
+     rm: str = getattr(config, "ram_rm") or "-r"
+     show: str = getattr(config, "ram_show") or "-s"
+     available: str = getattr(config, "ram_available") or "-v"
+
+******************************************************************************
 开始使用
---------
+******************************************************************************
+==============================================================================
+对于 PyPI 发行版来源
+==============================================================================
+| 建议使用 poetry
+|
 
-建议使用 poetry
+* 通过 poetry 添加到 NoneBot2 项目的 ``pyproject.toml``
 
-- 通过 poetry 添加到 ``nonebot2`` 项目的 ``pyproject.toml``
+.. code:: cmd
 
-.. code-block:: bash
+ poetry add nonebot-plugin-rauthman
 
-  poetry add nonebot-plugin-rauthman
+* 也可以通过 pip 从 `PyPI <https://pypi.org/project/nonebot-plugin-rauthman/>`_ 安装
 
-- 也可以通过 pip 从 `PyPI <https://pypi.org/project/nonebot-plugin-rauthman/>`_ 安装
+.. code:: cmd
 
-.. code-block:: bash
+ pip install nonebot-plugin-rauthman
 
-  pip install nonebot-plugin-rauthman
+* 参照下文在 NoneBot2 项目的环境文件 ``.env.*`` 中添加配置项
 
-- 在 ``nonebot2`` 项目中设置 ``nonebot.load_plugin()``
+==============================================================================
+对于 Git 来源
+==============================================================================
+| 自己看着办吧。
 
-    当使用 `nb-cli <https://github.com/nonebot/nb-cli>`_ 添加本插件时，该条会被自动添加
-
-.. code-block:: python
-
-  nonebot.load_plugin('nonebot_plugin_rauthman')
-
-- 参照下文在 ``nonebot2`` 项目的环境文件 ``.env.*`` 中添加配置项
-
+******************************************************************************
 配置项
---------
-
-- 授权管理信息保存位置（必须）：
-
-  ``savedata: str`` 保存相对路径，示例意为保存至运行目录下的 ``Yuni/savedata`` 目录
-
-.. code-block:: bash
-
-  savedata = Yuni/savedata
-
-- 授权管理应用策略（可选）：
-
-  ``0`` 根据可用功能授权，当功能在群聊的可用功能列表内时为可用（默认值）
-
-  ``1`` 根据功能级别授权，当群聊级别不低于功能所需级别时为可用
-
-.. code-block:: bash
-
-  auth_policy = 0
-
-- 授权管理指令所需的参数（可选）：
-
-  ``auth_command: str`` 指令名，默认为 ``auth``
-
-  ``auth_add: str`` 启用功能（根据可用功能授权），默认为 ``-a``
-
-  ``auth_rm: str`` 禁用功能（根据可用功能授权），默认为 ``-rm``
-
-  ``auth_show: str`` 展示群功能状态（根据可用功能授权），默认为 ``-s``
-
-  ``auth_available: str`` 展示全局可用功能（根据可用功能授权），默认为 ``-av``
-
-.. code-block:: bash
-
-  auth_command = auth
-  auth_add = -a
-  auth_rm = -rm
-  auth_show = -s
-  auth_available = -av
-
-- 为需要管理的 ``on_*`` 事件设置规则授权，示例意为将一个 ``on_command`` 事件划入一个名为 ``servicename`` 的功能，同时设置功能级别 ``1``
+******************************************************************************
+| 以下配置项皆为可选，即使不添加也可以直接使用默认值
 
 .. code-block:: python
+
+ # .env.prod
+ savedata = Yuni/savedata  # 保存路径，相对路径，此处为保存至运行目录下的 "Yuni/savedata/" 下，默认为 ""
+ ram_policy = 0  # 授权策略 0 为根据可用功能 1 为根据服务级别，默认为 0
+ ram_cmd = ram  # 指令名，或者叫触发词，默认为 ram
+ ram_add = -a  # 启用功能（根据可用功能），默认为 -a
+ ram_rm = -r  # 禁用功能（根据可用功能），默认为 -r
+ ram_show = -s  # 展示群功能状态（根据可用功能），默认为 -s
+ ram_available = -v  # 展示全局可用功能（根据可用功能），默认为 -v
+
+| 为需要管理的 ``on_*`` 事件设置规则授权，示例意为将一个 ``on_command`` 事件划入一个名为 ``module_name`` 的功能，同时设置功能级别 ``1``
+
+.. code:: python
 
   from nonebot.plugin import on_command
   from nonebot_plugin_rauthman import isInService
 
-  command = on_command('cmd', rule=isInService('servicename', 1))
- 
-- 这样，群聊必须被启用了该功能，或功能级别高于指定值（取决于当前应用的授权管理应用策略）才会进入事件处理
+  command = on_command('cmd', rule=isInService('module_name', 1))
 
+| 这样，群聊必须被启用了该功能，或功能级别高于 ``1`` 才会进入事件处理（取决于当前应用的授权管理应用策略）
+
+******************************************************************************
+小白案例
+******************************************************************************
+| 以 PyPI 发行版来源为例，基于以下配置文件和事件响应器
+
+.. code:: python
+
+ # .env.prod
+ ram_cmd = 功能  # 指令名，默认为 ram
+ ram_add = 开启  # 启用功能（根据可用功能），默认为 -a
+ ram_rm = 关闭  # 禁用功能（根据可用功能），默认为 -r
+ ram_show = 查询  # 展示群功能状态（根据可用功能），默认为 -s
+ ram_available = 全局查询  # 展示全局可用功能（根据可用功能），默认为 -v
+
+.. code:: python
+
+ from nonebot.plugin import on_notice
+
+ from nonebot_plugin_rauthman import isInService
+ notice = on_notice(rule=to_me() & isInService('戳一戳', 1))
+
+| 使用过程 `预览图 <BotTest1.jpg>`_
+
+******************************************************************************
+常见问题
+******************************************************************************
+* 这个插件可以做到什么？
+   | RAM 可以实现对不同群，不同功能的控制
+
+* 提示群聊未注册是怎么回事？
+   | 本地 JSON 文件中不存在该群群号，则会提示为群聊未注册
+   | 进行一次授权变更操作即可生成，如 ``ram 0``
+
+* 谁可以开启/关闭功能？
+   | ``SUPERUSERS`` 在 ``.env.*`` 中定义，参考 `基本配置 <https://v2.nonebot.dev/guide/basic-configuration.html#env-%E6%96%87%E4%BB%B6-2>`_
+
+* 批量对群进行授权修改？
+   | 私聊 Bot 直接发送 ``ram`` 并根据提示操作
+
+* 我设置了 ``ram_policy = 1``，怎么设置群 Level？
+   | 例如在 Bot 所在群聊中发送 ``ram 10``
+   | 这样这个群的 Level 就被设定成 ``10`` 默认的 Level 为 ``0``
+
+     授权修改操作与当前授权策略无关
+
+* 如果我希望在一个群中，管理员和群主可以修改开关/设置群 Level 我该怎么办？
+   | 对源代码第 ``104`` 行进行修改
+
+.. code:: python
+
+ permission: Permission = SUPERUSER  # 参考 NoneBot2 文档 - 进阶 - 权限控制
+
+******************************************************************************
 特别感谢
---------
+******************************************************************************
+* `Mrs4s / go-cqhttp <https://github.com/Mrs4s/go-cqhttp>`_
+* `nonebot / nonebot2 <https://github.com/nonebot/nonebot2>`_
+* `Sichongzou <https://github.com/Sichongzou>`_ 对 `README.md <README.md>`_ ``小白案例`` 和 ``常见问题`` 的贡献
 
-- `Mrs4s / go-cqhttp <https://github.com/Mrs4s/go-cqhttp>`_
-- `nonebot / nonebot2 <https://github.com/nonebot/nonebot2>`_
-
+******************************************************************************
 优化建议
---------
-
-如有优化建议请积极提交 Issues 或 Pull requests
+******************************************************************************
+| 如有优化建议请积极提交 Issues 或 Pull requests
---
+++ nonebot_plugin_rauthman/RAM.py
@@ -0,0 +1,483 @@
+'''
+Author       : Lancercmd
+Date         : 2021-12-17 09:45:45
+LastEditors  : Lancercmd
+LastEditTime : 2022-01-08 17:54:55
+Description  : None
+GitHub       : https://github.com/Lancercmd
+'''
+from __future__ import annotations
+
+from copy import deepcopy
+from dataclasses import dataclass, field
+from functools import wraps
+from os.path import join
+from pathlib import Path
+from sys import path as sys_path
+from typing import Optional, Union
+
+from loguru import logger
+from nonebot import get_driver
+from nonebot.adapters import Bot, Event, MessageTemplate
+from nonebot.adapters.onebot.v11 import Adapter as OneBot_V11_Adapter
+from nonebot.adapters.onebot.v11 import (FriendAddNoticeEvent,
+                                         FriendRecallNoticeEvent,
+                                         FriendRequestEvent, GroupMessageEvent,
+                                         MessageEvent, MetaEvent, NoticeEvent,
+                                         PrivateMessageEvent, RequestEvent)
+from nonebot.exception import ActionFailed
+from nonebot.params import State
+from nonebot.permission import SUPERUSER, Permission
+from nonebot.plugin import on_command
+from nonebot.rule import Rule
+from nonebot.typing import T_State
+
+from ._FileStation import FileStation
+from ._permission import onFocus
+
+config = get_driver().config
+
+
+def generate_savedata_path(id: int = None, *, flag: int = 0, _bot: Bot = None, _type: str = None) -> str:
+    """
+    ###   说明
+    -     获取指定个人或群聊 QQ 号的存档路径
+
+    ###   参数
+    -     id: int  指定全局，个人或群聊的 QQ 号，默认为全局
+    -     flag: int  在个人或群聊间切换，默认为个人，可选值如下：
+        -     0  个人
+        -     1  群聊
+    -     bot: Bot  当前 Bot 实例，优先于 type 默认为 None
+    -     type: str  指定 adapter 类型，默认为 None
+    """
+    _path = join(
+        sys_path[0], getattr(config, "savedata") or ""
+    )
+    if _bot:
+        _path = join(_path, _bot.type)
+    elif _type:
+        _path = join(_path, _type)
+    if id:
+        if flag == 0:
+            _path = join(_path, "private", f"{id}.json")
+        elif flag == 1:
+            _path = join(_path, "group", f"{id}.json")
+        else:
+            raise ValueError("UnknownFlag")
+    return _path
+
+
+@dataclass
+class RAM:
+    """
+    ##    RAM - 基于规则的授权管理
+    -     为 Matcher 配置一条或多条 Rule 来实现功能的授权管理
+
+    ###   当前已适配的 adapter 类型
+    -     OneBot V11 (CQHTTP) （仅控制群聊）
+    """
+    module_name: str = "RAM"
+    cqhttp: dict = field(default_factory=lambda: {"group": {}, "private": {}})
+    onebot_v11: dict = field(
+        default_factory=lambda: {"group": {}, "private": {}}
+    )
+
+
+@dataclass
+class Options:
+    """
+    ###   RAM - 配置项
+    ###   参数
+    -     cmd: str  指令名，或者叫触发词
+    -     policy: int  授权策略，可选值如下：
+        -     0  根据可用功能
+        -     1  根据服务级别
+
+    ###   参数 - 根据可用功能
+    -     add: str  启用功能
+    -     rm: str  禁用功能
+    -     show: str  展示群功能状态
+    -     available: str  展示全局可用功能
+    """
+    filepath: str = join(generate_savedata_path(), "global.json")
+    permission: Permission = SUPERUSER
+    policy: int = getattr(config, "ram_policy") or 0
+    cmd: str = getattr(config, "ram_cmd") or "ram"
+    add: str = getattr(config, "ram_add") or "-a"
+    rm: str = getattr(config, "ram_rm") or "-r"
+    show: str = getattr(config, "ram_show") or "-s"
+    available: str = getattr(config, "ram_available") or "-v"
+
+
+_opt = Options()
+
+
+class RAM_Control(FileStation):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(_opt.filepath, RAM().module_name, *args, **kwargs)
+
+    def load(self) -> dict:
+        super().load()
+        self.standardize()
+        return self.data
+
+    def reload(self, **kwargs) -> None:
+        super().reload(**kwargs)
+        self.standardize()
+
+    def standardize(self) -> None:
+        if not self.check_keys():
+            if not self._get("RAM"):
+                self.convert_from_legacy()
+            self.vacuum()
+        for key in RAM().__dataclass_fields__.keys():
+            setattr(self, key, self.get(key))
+
+    def check_keys(self) -> bool:
+        return self.keys() == RAM().__dataclass_fields__.keys()
+
+    def vacuum(self) -> None:
+        _base = RAM()
+        _base.cqhttp = self.get("cqhttp", RAM().cqhttp)
+        _base.onebot_v11 = self.get("onebot_v11", RAM().onebot_v11)
+        self.data = _base.__dict__
+
+    def applicator(func) -> None:
+        """
+        Decorator as an instance method.
+
+        Apply changes to the `self.data` dictionary.
+
+        Then apply changes to the particular `module_name` dictionary in `self._data`.
+
+        Finally, save the changes.
+        """
+        @wraps(func)
+        def wrapper(self: RAM_Control, *args, **kwargs) -> None:
+            func(self, *args, **kwargs)
+            self._update(self._module_name, self.data)
+            self.save()
+            self.reload(full=True)
+        return wrapper
+
+    def convert_from_legacy(self) -> None:
+        _path = Path(generate_savedata_path()) / "auth.json"
+        _fs = FileStation(_path)
+        if list(_fs._keys()) == list(RAM().cqhttp.keys()):
+            _base = RAM()
+            _base.cqhttp = _fs._data
+            self.data = _base.__dict__
+
+    _compatible_adapters = {OneBot_V11_Adapter.get_name(): "onebot_v11"}
+
+    def _check_adapter(self, bot: Bot) -> bool:
+        if bot.type in self._compatible_adapters:
+            self._base = getattr(
+                self, RAM_Control._compatible_adapters[bot.type]
+            )
+            return True
+        return False
+
+    @applicator
+    def set_universal(self: RAM_Control, bot: Bot, group_id: int, services: Optional[list[str]] = None, level: Optional[int] = None) -> None:
+        if self._check_adapter(bot):
+            _groups = self._base["group"]
+            if not f"{group_id}" in _groups:
+                _groups[f"{group_id}"] = {}
+            _home = _groups[f"{group_id}"]
+            if services:
+                cache = deepcopy(services)
+                if not "enabled" in _home:
+                    _home["enabled"] = []
+                if not "disabled" in _home:
+                    _home["disabled"] = []
+                _enabled = _home["enabled"]
+                _disabled = _home["disabled"]
+                if cache[0] == _opt.add:
+                    cache.remove(_opt.add)
+                    _valid = True
+                    for service in cache:
+                        if not service in available:
+                            _valid = False
+                    if _valid:
+                        _enabled += cache
+                        for service in _disabled:
+                            if service in _enabled:
+                                _disabled.remove(service)
+                    if not _disabled:
+                        del _home["disabled"]
+                elif cache[0] == _opt.rm:
+                    cache.remove(_opt.rm)
+                    _valid = True
+                    for service in cache:
+                        if not service in available:
+                            _valid = False
+                    if _valid:
+                        _disabled += cache
+                        for service in _enabled:
+                            if service in _disabled:
+                                _enabled.remove(service)
+                    if not _enabled:
+                        del _home["enabled"]
+                if "enabled" in _home:
+                    _home["enabled"] = list(set(_enabled))
+                    _home["enabled"].sort()
+                if "disabled" in _home:
+                    _home["disabled"] = list(set(_disabled))
+                    _home["disabled"].sort()
+            else:
+                _home["level"] = level
+
+    def check_universal(self: RAM_Control, bot: Bot, group_id: int, service: Optional[str] = None) -> Union[bool, int]:
+        if self._check_adapter(bot):
+            _groups = self._base["group"]
+            if not f"{group_id}" in _groups:
+                return 0
+            elif service:
+                if not "enabled" in _groups[f"{group_id}"]:
+                    return 0
+                else:
+                    return bool(service in _groups[f"{group_id}"]["enabled"])
+            elif not "level" in _groups[f"{group_id}"]:
+                return 0
+            else:
+                return _groups[f"{group_id}"]["level"]
+        else:
+            return 0
+
+    def show_universal(self: RAM_Control, bot: Bot, group_id: int) -> dict:
+        data = {}
+        if self._check_adapter(bot):
+            _groups = self._base["group"]
+            if f"{group_id}" in _groups:
+                for i in _groups[f"{group_id}"]:
+                    data[i] = _groups[f"{group_id}"][i]
+        return data
+
+
+_amc = RAM_Control()
+worker = on_command(_opt.cmd, permission=_opt.permission)
+
+
+@worker.permission_updater
+async def _(event: Event) -> Permission:
+    return await onFocus(event)
+
+
+@worker.handle()
+async def _(event: GroupMessageEvent, state: T_State = State()) -> None:
+    state["group_id"] = str(event.group_id)
+    if event.get_plaintext(state):
+        state["services"] = event.get_plaintext(state)
+
+
+@worker.handle()
+async def _(event: PrivateMessageEvent, state: T_State = State()) -> None:
+    actions = event.get_plaintext(state).split(" ", 1)
+    if len(actions) == 1:
+        if actions[0] == _opt.available:
+            state["group_id"] = "0"
+            state["services"] = _opt.available
+        elif actions[0] == _opt.show:
+            state["services"] = _opt.show
+    elif actions[0] == _opt.show:
+        if actions[1].isdigit():
+            state["group_id"] = actions[1]
+        state["services"] = _opt.show
+
+
+@worker.handle()
+async def _(event: Event, state: T_State = State()) -> None:
+    supported = \
+        isinstance(event, MessageEvent)
+    if supported:
+        state["add"] = _opt.add
+        state["rm"] = _opt.rm
+        state["show"] = _opt.show
+        state["available"] = _opt.available
+        state["prompt"] = "请输入需要操作的群号，并用空格隔开~"
+    else:
+        logger.warning("Not supported: RAM")
+
+
+@worker.got("group_id", prompt=MessageTemplate("{prompt}"))
+async def _(event: MessageEvent, state: T_State = State()) -> None:
+    try:
+        _input = str(state["group_id"])
+        _group_id = _input.split(" ")
+        group_ids = []
+        for i in _group_id:
+            if i.isdigit():
+                if not i in group_ids and i != "1":
+                    group_ids.append(int(i))
+                else:
+                    await worker.finish("请不要用此方式进行全局设置哦~")
+            else:
+                await worker.finish("Invalid input")
+        if group_ids:
+            state["group_ids"] = group_ids
+        else:
+            await worker.finish("Invalid input")
+    except ActionFailed as e:
+        logger.warning(
+            f"ActionFailed {e.info['retcode']} {e.info['msg'].lower()} {e.info['wording']}"
+        )
+        return
+    state["prompt"] = "\n".join([
+        "请继续输入——",
+        f"{state['add']} sv1 sv2 ... | 启用功能",
+        f"{state['rm']} sv1 sv2 ... | 禁用功能",
+        f"{state['show']} | 查看群功能状态",
+        f"{state['available']} | 展示全局可用功能",
+        "※ 设置群级别请直接发送数字"
+    ])
+
+
+@worker.got("services", prompt=MessageTemplate("{prompt}"))
+async def _(bot: Bot, event: MessageEvent, state: T_State = State()) -> None:
+    try:
+        _input = str(state["services"])
+        _services = _input.split(" ")
+        if len(_services) == 1:
+            if _services[0].isdigit():
+                if int(_services[0]) > 99999999999999999999:
+                    await worker.finish(f"Level too large: {_services[0]}")
+                segments = []
+                for group_id in state["group_ids"]:
+                    prev = _amc.check_universal(bot, group_id)
+                    _amc.set_universal(bot, group_id, level=int(_services[0]))
+                    if len(state["group_ids"]) == 1:
+                        segments.append(
+                            f"群 Level {prev} => {event.get_plaintext(state)}"
+                        )
+                    else:
+                        segments.append(
+                            f"群 {group_id} Level {prev} => {event.get_plaintext(state)}"
+                        )
+                await worker.finish("\n".join(segments))
+            elif _services[0] == _opt.show:
+                queue = []
+                for group_id in state["group_ids"]:
+                    status = _amc.show_universal(bot, group_id)
+                    if status:
+                        keys = list(status.keys())
+                        layout = []
+                        _status = []
+                        for i in keys:
+                            if isinstance(status[i], list):
+                                _status.append(
+                                    " ".join(status[i]).replace(",", "")
+                                )
+                            else:
+                                _status.append(status[i])
+                        for i in range(len(keys)):
+                            if keys[i] == "enabled":
+                                if _status:
+                                    layout.append(f"当前启用：{_status[i]}")
+                                else:
+                                    layout.append(f"当前启用：无")
+                            elif keys[i] == "level":
+                                layout.append(f"功能级别：{_status[i]}")
+                            else:
+                                layout.append(f"{keys[i]}: {_status[i]}")
+                        queue.append(f"{group_id} " + " | ".join(layout))
+                    else:
+                        queue.append(f"{group_id} " + "群聊未注册")
+                    if len(queue) > 9:
+                        break
+                await worker.finish("\n".join(queue))
+            elif _services[0] == _opt.available:
+                await worker.finish("".join(["全局可用：", " ".join(available)]))
+            else:
+                await worker.finish("Invalid input")
+        elif _services[0] == _opt.add:
+            for group_id in state["group_ids"]:
+                _amc.set_universal(bot, group_id, _services)
+            _services.remove(_opt.add)
+            invalid = []
+            for i in _services:
+                if not i in available:
+                    invalid.append(i)
+            for i in invalid:
+                _services.remove(i)
+            message = []
+            if _services:
+                message.append(f"已启用：{' '.join(_services)}")
+            if invalid:
+                message.append(f"未找到：{' '.join(invalid)}")
+            await worker.finish("\n".join(message))
+        elif _services[0] == _opt.rm:
+            for group_id in state["group_ids"]:
+                _amc.set_universal(bot, group_id, _services)
+            _services.remove(_opt.rm)
+            invalid = []
+            for i in _services:
+                if not i in available:
+                    invalid.append(i)
+            for i in invalid:
+                _services.remove(i)
+            message = []
+            if _services:
+                message.append(f"已禁用：{' '.join(_services)}")
+            if invalid:
+                message.append(f"未找到：{' '.join(invalid)}")
+            await worker.finish("\n".join(message))
+        else:
+            logger.warning(f"Invalid input: {_services}")
+    except ActionFailed as e:
+        logger.warning(
+            f"ActionFailed {e.info['retcode']} {e.info['msg'].lower()} {e.info['wording']}"
+        )
+available = []
+warning = False
+
+
+def isInService(service: Optional[str] = None, level: Optional[int] = None) -> Rule:
+    """
+    ###   RAM - Rule
+    -     为 Matcher 配置一条或多条 Rule 来实现功能的授权管理
+    -     通过下列方法之一，判断对应功能是否可用
+      -     功能是否对群聊启用
+      -     群聊是否满足级别
+
+    ###   参数
+    -     service: str  功能名称
+    -     level: int  功能级别
+    """
+    global warning
+    if service and not service in available:
+        if " " in service and not warning:
+            logger.warning("At least 1 space found in the service name")
+            warning = True
+        available.append(service)
+
+    async def _isInService(bot: Bot, event: Event) -> bool:
+        if isinstance(event, MessageEvent):
+            if isinstance(event, GroupMessageEvent):
+                if service and _opt.policy == 0:
+                    return _amc.check_universal(bot, getattr(event, "group_id"), service)
+                elif level and _opt.policy == 1:
+                    return _amc.check_universal(bot, getattr(event, "group_id")) >= level
+            else:
+                return True
+        elif isinstance(event, NoticeEvent):
+            if isinstance(event, FriendAddNoticeEvent) or isinstance(event, FriendRecallNoticeEvent):
+                return True
+            elif service and _opt.policy == 0:
+                return _amc.check_universal(bot, getattr(event, "group_id"), service) if getattr(event, "group_id") else False
+            elif level and _opt.policy == 1:
+                return _amc.check_universal(bot, getattr(event, "group_id")) >= level if getattr(event, "group_id") else False
+        elif isinstance(event, RequestEvent):
+            if isinstance(event, FriendRequestEvent):
+                return True
+            elif service and _opt.policy == 0:
+                return _amc.check_universal(bot, getattr(event, "group_id"), service) if getattr(event, "group_id") else False
+            elif level and _opt.policy == 1:
+                return _amc.check_universal(bot, getattr(event, "group_id")) >= level if getattr(event, "group_id") else False
+        elif isinstance(event, MetaEvent):
+            return True
+        else:
+            logger.warning("Not supported: RAM")
+            return True
+    return Rule(_isInService)
